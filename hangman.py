9b3d0ab1a73566dcb6962fbf05af4ab572cf1bac
fredrikj31 <fredrik1206@gmail.com>
2019-12-22 11:40:53 +0100

    Created starter files

---
+++ hangman.py
@@ -0,0 +1,31 @@
+from levels import levels
+
+class Hangman:
+	
+	Level1 = """
+  ______
+  |    |      Phrase #: {}
+  |    o      Guessed : {}
+  |   /|\     Average :  50%
+  |    |
+  |   /
+ _|_
+|   |______
+|          |
+|__________|
+ 
+Phrase: {}
+	"""
+
+	def startGame(self, Word):
+		print(self.gameLayout)
+		pass
+
+	def pickWordFromList(self):
+		pass
+
+	def detectWon():
+		pass
+
+	def splitWord():
+		pass
---
+++ program.py
@@ -0,0 +1,5 @@
+from hangman import Hangman
+
+hangman = Hangman()
+
+hangman.startGame("Hej")
